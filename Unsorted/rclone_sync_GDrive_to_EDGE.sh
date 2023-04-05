f7556592fa566bd13a9074c334fd0616e388efab
iverlein <iverlein@gmail.com>
2023-04-05 16:19:54 +0300

    Added --progress flag

--- Unsorted/rclone_sync_GDrive_to_EDGE.sh
+++ Unsorted/rclone_sync_GDrive_to_EDGE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo rclone sync Iverlein_Google_Drive:Documents/GNUCash /home/iverlein/Documents/GNUCash 
-sudo rclone sync Iverlein_Google_Drive:Documents/KeePassDB /home/iverlein/Documents/KeePassDB
+sudo rclone sync --progress Iverlein_Google_Drive:Documents/GNUCash /home/iverlein/Documents/GNUCash 
+sudo rclone sync --progress Iverlein_Google_Drive:Documents/KeePassDB /home/iverlein/Documents/KeePassDB
